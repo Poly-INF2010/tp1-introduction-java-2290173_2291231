a8d93b3316da2248d2c26e3c28fbe259c09ba43f
Guillaume <guillaumegauthier3113@gmail.com>
2024-02-04 18:02:58 -0500

    les shapes fini

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -5,29 +5,39 @@ import Point.Point2d;
 import java.util.Collection;
 
 public class Ellipse extends BaseShape {
-    /** TODO
+    /**
+     * TODO
      * Create a filled Ellipse that is centered on (0, 0)
-     * @param widthDiameter Width of the Ellipse
+     * @param widthDiameter  Width of the Ellipse
      * @param heightDiameter Height of the Ellipse
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
 
+        double a = widthDiameter / 2;
         double b = heightDiameter / 2;
 
-         for (double a = widthDiameter / 2; a > 0 && b > 0; a -= 0.5){
+        double perimetre = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
+        double step = 2 * Math.PI / (perimetre / 0.5);
 
-             double perimetre = widthDiameter * Math.PI;
-             int step = (int) (2 * Math.PI / perimetre / 0.5);
+        for (double angle = 0; angle < 2 * Math.PI; angle += step) {
+            double x = a * Math.cos(angle);
+            double y = b * Math.sin(angle);
+            this.add(new Point2d(x, y));
+        }
 
-             for (double angle = 0; angle < 2 * Math.PI; angle += step){
-                 this.add(new Point2d(Math.cos(angle) * a, Math.sin(angle) * b));
-             }
-             b -= 0.5;
-         }
+        for (double y = -b + 0.5; y < b; y += 0.5) {
+            for (double x = -a + 0.5; x < a; x += 0.5) {
+                if ((x * x) / (a * a) + (y * y) / (b * b) <= 1.0) {
+                    this.add(new Point2d(x, y));
+                }
+            }
+        }
     }
 
-    /** TODO
+    /**
+     * TODO
      * Create a filled Ellipse that is centered on (0,0)
+     *
      * @param dimensions 2D point containing the width and height of the Ellipse
      */
     public Ellipse(Point2d dimensions) {
@@ -36,13 +46,16 @@ public class Ellipse extends BaseShape {
 
     /**
      * Create an Ellipse from a given collection of 2D points
+     *
      * @param coords Collection of 2D points
      */
     private Ellipse(Collection<Point2d> coords) {
 
     }
 
-    /** TODO
+    /**
+     * TODO
+     *
      * @return Deep Copy of the Ellipse
      */
     @Override
