642e1e68005e6bbe2bb8f7e4c674ae04446efaaf
Guillaume <guillaumegauthier3113@gmail.com>
2024-02-05 00:47:34 -0500

    les shapes fini

--- src/main/java/Shape/Ellipse.java
+++ src/main/java/Shape/Ellipse.java
@@ -10,23 +10,23 @@ public class Ellipse extends BaseShape {
      * Create a filled Ellipse that is centered on (0, 0)
      * @param widthDiameter  Width of the Ellipse
      * @param heightDiameter Height of the Ellipse
+     *
+     *                       double perimetre = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
+     *         double step = 2 * Math.PI / (perimetre / 0.5);
+     *
+     *         for (double angle = 0; angle < 2 * Math.PI; angle += step) {
+     *             double x = a * Math.cos(angle);
+     *             double y = b * Math.sin(angle);
+     *             this.add(new Point2d(x, y));
+     *         }
      */
     public Ellipse(Double widthDiameter, Double heightDiameter) {
 
         double a = widthDiameter / 2;
         double b = heightDiameter / 2;
 
-        double perimetre = 2 * Math.PI * Math.sqrt((a * a + b * b) / 2);
-        double step = 2 * Math.PI / (perimetre / 0.5);
-
-        for (double angle = 0; angle < 2 * Math.PI; angle += step) {
-            double x = a * Math.cos(angle);
-            double y = b * Math.sin(angle);
-            this.add(new Point2d(x, y));
-        }
-
-        for (double y = -b + 0.5; y < b; y += 0.5) {
-            for (double x = -a + 0.5; x < a; x += 0.5) {
+        for (double y = -b; y < b; y += 0.5) {
+            for (double x = -a; x < a; x += 0.5) {
                 if ((x * x) / (a * a) + (y * y) / (b * b) <= 1.0) {
                     this.add(new Point2d(x, y));
                 }
