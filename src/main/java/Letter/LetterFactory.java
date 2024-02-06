32aef2496a78cd89be499bff52d6a629a79f8a69
William Stapinsky <wstapinsky@outlook.fr>
2024-02-05 22:42:29 -0500

    Cleanup fichier LetterFactory

--- src/main/java/Letter/LetterFactory.java
+++ src/main/java/Letter/LetterFactory.java
@@ -4,13 +4,23 @@ import Point.Point2d;
 import Shape.*;
 
 public final class LetterFactory {
-    final static Double maxHeight = 100.0;
+    final static Double maxHeight = 150.0;
     final static Double maxWidth = maxHeight / 2.5;
     final static Double halfMaxHeight = maxHeight / 2;
     final static Double halfMaxWidth = maxWidth / 2;
     final static Double stripeThickness = maxHeight / 8;
     final static Double halfStripeThickness = stripeThickness / 2;
 
+
+    final static Double angleA = 12.0;
+    final static Double topAndBottomBarPositionE = 2.15;
+    final static Double barLengthScalarE = 0.8;
+    final static Double circleBPositionScalar = 1.25;
+    final static Double hollowCircleWidth = 0.1;
+    final static Double angleN = 160.0;
+    final static Double middleBarNScalar = 1.05;
+    final static Double bCircleSize = maxHeight / 4;
+    final static Double hollowedBCircleSize = maxHeight / 5;
 
     /**
      * Create the letter A graphically
@@ -19,11 +29,11 @@ public final class LetterFactory {
     public static BaseShape create_A()  {
 
         var letterA = new Rectangle(halfMaxWidth, halfStripeThickness);
-        var leftBar = new Rectangle(halfMaxWidth/8, maxHeight);
+        var leftBar = new Rectangle(halfStripeThickness, maxHeight);
         var rightBar = leftBar.clone();
 
-        leftBar.rotate(leftBar.getCoords(), Math.toRadians(170));
-        rightBar.rotate(rightBar.getCoords(), Math.toRadians(10));
+        leftBar.rotate(leftBar.getCoords(), Math.toRadians(180-angleA));
+        rightBar.rotate(rightBar.getCoords(), Math.toRadians(angleA));
 
         leftBar.translate(leftBar.getCoords(), new Point2d(halfMaxWidth/2, 0.0));
         rightBar.translate(rightBar.getCoords(), new Point2d(-halfMaxWidth/2, 0.0));
@@ -39,15 +49,15 @@ public final class LetterFactory {
      */
     public static BaseShape create_B() {
 
-        var letterB = new Rectangle(halfMaxWidth/2, maxHeight);
+        var letterB = new Rectangle(stripeThickness, maxHeight);
         letterB.translate(letterB.getCoords(), new Point2d(-halfMaxWidth, 0.0));
 
-        var hollowCircle = new Circle(maxHeight/4);
-        hollowCircle.remove(new Circle(maxHeight/5));
+        var hollowCircle = new Circle(bCircleSize);
+        hollowCircle.remove(new Circle(hollowedBCircleSize));
         var topCircle = hollowCircle.clone();
 
-        hollowCircle.translate(hollowCircle.getCoords(), new Point2d(0.0, halfMaxWidth*1.25));
-        topCircle.translate(topCircle.getCoords(), new Point2d(0.0, -halfMaxWidth*1.25));
+        hollowCircle.translate(hollowCircle.getCoords(), new Point2d(0.0, halfMaxWidth*circleBPositionScalar));
+        topCircle.translate(topCircle.getCoords(), new Point2d(0.0, -halfMaxWidth*circleBPositionScalar));
 
         letterB.add(hollowCircle);
         letterB.add(topCircle);
@@ -61,10 +71,10 @@ public final class LetterFactory {
      */
     public static BaseShape create_C() {
 
-        var letterC = new Ellipse(maxWidth, maxHeight).remove(new Ellipse(maxWidth*0.9, maxHeight*0.9));
+        var letterC = new Ellipse(maxWidth, maxHeight).remove(new Ellipse(maxWidth*(1-hollowCircleWidth), maxHeight*(1-hollowCircleWidth)));
 
         var blockRemove = new Square(maxHeight/1.5);
-        blockRemove.translate(blockRemove.getCoords(), new Point2d(halfMaxWidth, 0.0));
+        blockRemove.translate(blockRemove.getCoords(), new Point2d(maxWidth, 0.0));
         letterC.remove(blockRemove);
 
         return letterC;
@@ -75,15 +85,15 @@ public final class LetterFactory {
      * @return BaseShape containing the letter E
      */
     public static BaseShape create_E() {
-        var letterE = new Rectangle(halfMaxWidth/4, maxHeight);
+        var letterE = new Rectangle(halfStripeThickness, maxHeight);
 
-        var midBar = new Rectangle(maxWidth*0.8, halfMaxHeight/8);
+        var midBar = new Rectangle(maxWidth*barLengthScalarE, halfStripeThickness);
         letterE.translate(letterE.getCoords(), new Point2d(-halfMaxWidth, 0.0));
 
         var topBar = midBar.clone();
-        topBar.translate(topBar.getCoords(), new Point2d(0.0, -maxHeight/2.15));
+        topBar.translate(topBar.getCoords(), new Point2d(0.0, -maxHeight/topAndBottomBarPositionE));
         var bottomBar = midBar.clone();
-        bottomBar.translate(bottomBar.getCoords(), new Point2d(0.0, maxHeight/2.1));
+        bottomBar.translate(bottomBar.getCoords(), new Point2d(0.0, maxHeight/topAndBottomBarPositionE));
 
         letterE.add(topBar);
         letterE.add(bottomBar);
@@ -97,10 +107,10 @@ public final class LetterFactory {
      * @return BaseShape containing the letter H
      */
     public static BaseShape create_H() {
-        var letterH = new Rectangle(halfMaxWidth/4, maxHeight);
+        var letterH = new Rectangle(halfStripeThickness, maxHeight);
         var rightBar = letterH.clone();
 
-        var midBar = new Rectangle(maxWidth*0.8, halfMaxHeight/8);
+        var midBar = new Rectangle(maxWidth, halfStripeThickness);
 
         letterH.translate(letterH.getCoords(), new Point2d(-halfMaxWidth, 0.0));
         rightBar.translate(rightBar.getCoords(), new Point2d(halfMaxWidth, 0.0));
@@ -116,14 +126,14 @@ public final class LetterFactory {
      * @return BaseShape containing the letter N
      */
     public static BaseShape create_N() {
-        var letterN = new Rectangle(halfMaxWidth/4, maxHeight);
+        var letterN = new Rectangle(halfStripeThickness, maxHeight);
         var rightBar = letterN.clone();
 
-        var midBar = new Rectangle(halfMaxWidth/4, maxHeight*1.05);
+        var midBar = new Rectangle(halfStripeThickness, maxHeight*middleBarNScalar);
 
         letterN.translate(letterN.getCoords(), new Point2d(-halfMaxWidth, 0.0));
         rightBar.translate(rightBar.getCoords(), new Point2d(halfMaxWidth, 0.0));
-        midBar.rotate(midBar.getCoords(), Math.toRadians(160));
+        midBar.rotate(midBar.getCoords(), Math.toRadians(angleN));
 
         letterN.add(rightBar);
         letterN.add(midBar);
@@ -136,7 +146,8 @@ public final class LetterFactory {
      * @return BaseShape containing the letter O
      */
     public static BaseShape create_O() {
-        return new Ellipse(maxWidth, maxHeight).remove(new Ellipse(maxWidth*0.9, maxHeight*0.9));
+        return new Ellipse(maxWidth, maxHeight)
+                .remove(new Ellipse(maxWidth*(1-hollowCircleWidth), maxHeight*(1-hollowCircleWidth)));
     }
 
 }
